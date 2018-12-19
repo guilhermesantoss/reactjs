65c22ba2317bd3f50c5a7f5d87b8b0dfc3bdc164
Guilherme Santos TED CommandLine <guilherme.santos@tlsbr.com>
2018-12-19 15:46:36 -0200

    att lista de usuarios do banco de dados feita com alteração e exclusão

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -17,6 +17,12 @@ const initialState = {
 export default class UserCrud extends Component {
     state = { ...initialState };
 
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data });
+        });
+    }
+
     clear() {
         this.setState({ user: initialState.user });
     }
@@ -32,9 +38,9 @@ export default class UserCrud extends Component {
             });
     }
 
-    getUpdatedList(user) {
+    getUpdatedList(user, add = true) {
         const list = this.state.list.filter(u => u.id !== user.id);
-        list.unshift(user);
+        if(user) list.unshift(user);
         return list;
     }
 
@@ -70,8 +76,8 @@ export default class UserCrud extends Component {
                 <hr />
                 <div className="row">
                     <div className="col-12 d-flex justify-content-end">
-                        <buton className="btn btn-primary"
-                            onClick={e => this.save(e)}>Salvar</buton>
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>Salvar</button>
                         <button className="btn btn-secundary ml-2"
                             onClick={e => this.clear(e)}>Cancelar</button>
                     </div>
@@ -80,10 +86,62 @@ export default class UserCrud extends Component {
         );
     }
 
+    load(user) {
+        this.setState({ user });
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false);
+            this.setState({ list });
+        });
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        );
+    }
+
+    renderRows() {
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning"
+                            onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2"
+                            onClick={() => this.remove(user)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            );
+        });
+    }
+
     render() {
         return (
             <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         );
     }
